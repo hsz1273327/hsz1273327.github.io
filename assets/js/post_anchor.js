246e8c8d04df0a5077f6d7f614bb56fe4b5b2a2e
HUANG SIZHE <hsz1273327@gmail.com>
2020-11-19 16:58:56 +0800

    update

--- assets/js/post_anchor.js
+++
@@ -1,8 +0,0 @@
-async_load("http://cdn.bootcss.com/anchor-js/1.1.1/anchor.min.js",function(){
-    anchors.options = {
-        visible: 'always',
-        placement: 'left',
-        icon: '¶'
-    };
-    anchors.add().remove('.intro-header h1').remove('.subheading').remove('.sidebar-container h5');
-})
