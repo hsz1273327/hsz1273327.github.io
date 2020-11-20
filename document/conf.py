629504d79e475f89ff6c3173bec54e750fb9aa62
HUANG SIZHE <hsz1273327@gmail.com>
2020-11-20 19:53:18 +0800

    update

--- document/conf.py
+++ document/conf.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 import sphinx_rtd_theme
 from recommonmark.transform import AutoStructify
 from recommonmark.parser import CommonMarkParser
@@ -11,6 +13,7 @@ release = '0.0.0'
 
 # 使用的插件
 extensions = [
+    'sphinx_rtd_theme',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
@@ -35,7 +38,7 @@ source_suffix = {
 
 
 # 指定编译成html时使用的主题
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # 指定编译成html时使用的静态文件所在位置
 html_static_path = ['_static']
@@ -44,12 +47,14 @@ html_static_path = ['_static']
 # todo插件的设置
 todo_include_todos = True
 
-# 主题
-extensions.append('sphinx_rtd_theme')
-html_theme = "sphinx_rtd_theme"
-html_theme_options = {
-    'logo_only': True,
-    'navigation_depth': 5,
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+        'relations.html',
+        'searchbox.html',
+        'donate.html',
+    ]
 }
 
 
@@ -61,10 +66,11 @@ source_suffix[".md"] = 'markdown'
 
 # 设置markdown渲染器的自定义项
 
+
 def setup(app):
     github_doc_root = 'https://localhost:5000'
     app.add_config_value('recommonmark_config', {
-        #'url_resolver': lambda url: github_doc_root + url,
+        # 'url_resolver': lambda url: github_doc_root + url,
         "enable_auto_toc_tree": True,
         "auto_toc_tree_section": "目录",
         'auto_toc_maxdepth': 2,
@@ -73,3 +79,26 @@ def setup(app):
     }, True)
 
     app.add_transform(AutoStructify)
+
+
+locale_dirs = ['locale/']   #
+gettext_compact = False     # optional.
+
+
+# 多语言支持
+try:
+    html_context
+except:
+    html_context = dict()
+html_context['display_lower_left'] = True
+
+# 从环境变量`CURRENT_LANGUAGE`获取当前语言,默认为zh_CN
+current_language = os.environ.get('CURRENT_LANGUAGE') if os.environ.get('CURRENT_LANGUAGE') else 'zh_CN'
+html_context['current_language'] = current_language
+
+# # POPULATE LINKS TO OTHER LANGUAGES
+html_context['languages'] = [('zh_CN', '/')]
+
+languages = [lang.name for lang in Path(__file__).parent.joinpath("locale").iterdir() if lang.is_dir()]
+for lang in languages:
+    html_context['languages'].append((lang, f'/{lang}/'))
