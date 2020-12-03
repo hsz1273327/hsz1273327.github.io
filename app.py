7be484f13c2c1772499135d38efd35690c96b11a
HUANG SIZHE <hsz1273327@gmail.com>
2020-12-03 16:43:54 +0800

    update

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ from sanic.response import HTTPResponse, json
 from aredis import StrictRedis
 
 
-
-
 async def getfoo(request: Request) -> HTTPResponse:
     value = await request.app.redis.get('foo')
     return json({"result": value})
 
+
 async def ping(_: Request) -> HTTPResponse:
+    print("inside")
     return json({"result": "pong"})
 
 
@@ -28,17 +28,22 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if args.authclient:
         # 双向校验
-        context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
+        print("双向校验")
+        context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
         # 设置服务证书和私钥
-        context.load_cert_chain("/path/to/cert", keyfile="/path/to/keyfile")
+        context.load_cert_chain("./serverkey/server-cert.pem", keyfile="./serverkey/server-key.pem")
         # 设置根证书
-        context.load_verify_locations('./path/to/caroot.crt')
+        context.load_verify_locations('./ca/ca.pem')
+        # 强制双向认证
+        context.verify_mode = ssl.CERT_REQUIRED
+        # context.post_handshake_auth = True
 
     else:
         # 单向校验
-        context = ssl.create_default_context(purpose=ssl.Purpose.SERVICE_AUTH)
+        #context = ssl.create_default_context(purpose=ssl.Purpose.SERVER_AUTH)
+        context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
         # 设置服务令牌和私钥
-        context.load_cert_chain("/path/to/cert", keyfile="/path/to/keyfile")
+        context.load_cert_chain("./serverkey/server-cert.pem", keyfile="./serverkey/server-key.pem")
     app = Sanic("hello_example")
     client = StrictRedis.from_url(args.redis_url, decode_responses=True)
     app.redis = client
--- cli.py
+++ cli.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     parser.add_argument('--authclient', action='store_true', default=False, help='是否双向认证')
     args = parser.parse_args()
     if args.authclient:
-        res = rq.get('https://localhost:5000/ping', verify='/path/to/certfile',cert=('/path/client.cert', '/path/client.key'))
+        res = rq.get('https://localhost:5000/ping', verify='./ca/ca.pem', cert=('./clientkey/client-cert.pem', './clientkey/client-key.pem'))
     else:
-        res = rq.get('https://localhost:5000/pong', verify='/path/to/certfile')
+        res = rq.get('https://localhost:5000/ping', verify='./ca/ca.pem')
+    print(res.status_code)
     print(res.json())
